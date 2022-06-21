7bfa3e4d7c38d0c60e5a3a5b834f8a694334aff5
KIM JI HYEON <kwlgus0318@gmail.com>
2022-06-21 15:46:58 +0900

    20220621

--- index.js
+++ index.js
@@ -1,38 +1,11 @@
-const slideContainer = document.getElementById('slide');
-const slideItems = Array.from(document.querySelectorAll("#slide li"));
-const left = document.getElementById('left');
-const right = document.getElementById('right');
-let current = 0;
-function displayControl(arr) {
-arr.forEach((value) => {
-  let getDatasetValue = Number(value.dataset.slideItem);
-  if (getDatasetValue === current) {
-  value.style.display = "block";
-  } else {
-  value.style.display = "none";
-  }
-});
-}
-displayControl(slideItems);
-function buttonEvent(eventTrigger, targetElement) {
-eventTrigger.addEventListener('click', function () {
-  if (eventTrigger === left) {
-    current--;
-    if (current < 0) {
-      current = targetElement.length - 1;
-    }
-    displayControl(targetElement);
-  }
-  if (eventTrigger === right) {
-    current++;
-    if (current >= targetElement.length) {
-      current = 0;
-    }
-    displayControl(targetElement);
-  }
-});
-};
+const menu = document.querySelector('.menu>li');
+console.log(menu)
 
-buttonEvent(left, slideItems);
-buttonEvent(right, slideItems);
+const menuHover = document.querySelector('.menu-hover');
+
+menu,addEventListener('mouseover', ()=>{
+
+}
+)
+
 
